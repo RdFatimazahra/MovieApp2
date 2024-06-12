95d1f1fb51aabddf11384e11d07ef22f7965325a
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-06-12 16:07:03 +0100

    Création model movie

--- src/main/java/org/movieapp/movieapp2/model/Movie.java
+++ src/main/java/org/movieapp/movieapp2/model/Movie.java
@@ -1,4 +1,31 @@
 package org.movieapp.movieapp2.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  int idMovie;
+    private  String  titre;
+    private String dateSortie;
+    private  String genre;
+    private  String  realisateur;
+    private int note;
+
+
 }
